203a82aeead5712a440aa96f596e5d5f5b888d0d
Glenn Mizzi <234417267+gemizzi@users.noreply.github.com>
2025-09-30 23:54:54 +1000

    Add status text to display the next turn

--- src/Board.js
+++ src/Board.js
@@ -11,6 +11,7 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  let status = "Next player: " + (xIsNext ? "X" : "O");
 
   function handleClick(i) {
     if (squares[i]) {
@@ -31,6 +32,7 @@ export default function Board() {
 
   return (
     <div className="board">
+      <div className="board-status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
